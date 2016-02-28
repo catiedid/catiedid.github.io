dc41a2b0429933d0e7387c2041e49a8150a8034f
catiedid <catie.collins@gmail.com>
2016-02-28 14:58:04 -0500

    Added styling and content

---
+++ script.js
@@ -0,0 +1,36 @@
+window.onload=function(e){
+alert("Thanks for visiting my site!");
+var kittens = 5;
+function turtleFact () {
+	alert("Some turtles can live longer than humans");
+}
+
+turtleFact();
+
+function temptGang(gangName) {
+	alert(gangName + "Come out and play")
+}
+
+function addNumbers(num1, num2) {
+	alert(num1+num2)
+}
+
+// addNumbers(10, 5);
+
+function square(number) {
+	return number * number;
+}
+
+function displaySquare(squaredNumber) {
+	console.log("Your number is " + squaredNumber);
+}
+
+displaySquare(square(3));
+
+
+var pageNode = document.getElementsByTagName('body')[0];
+pageNode.innerHTML = '<h1>Oh Noes!</h1> <p>I just changed the whole page!</p>'
+}
+
+var title = document.getElementbyID('open-title');
+title.innerHTML = "Give me all of the bacon and eggs you have.";
